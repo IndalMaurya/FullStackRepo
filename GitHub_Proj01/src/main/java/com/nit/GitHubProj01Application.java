8d0e2185089c29256dbc28543a3246162c80deec
HP <HP@DESKTOP-U3966QN>
2024-11-17 12:24:22 +0530

    TL - Project Directory Created By TL

---
+++ GitHub_Proj01/src/main/java/com/nit/GitHubProj01Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubProj01Application.class, args);
+	}
+
+}
